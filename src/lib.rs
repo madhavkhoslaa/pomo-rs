c4c094b4baaeec84a3cf6e46b16daffe3a17fcd7
Madhav Khosla <madhav.khoslaa@gmail.com>
2022-09-01 19:16:21 +0530

    Base code for start of pomodoro

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,110 @@
-struct InterfacePomodoro {
-    pub work_time_minutes: u8,
-    pub break_time_minutes: u8,
+use symlink;
+use clap:: { Parser };
+use notify_rust::*;
+
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct InterfacePomodoro {
+    
+    /// This argument takes in the work_time 
+    #[clap(short, long)]
+    pub work_time: u8,
+
+    /// This argument takes in the break_time
+    #[clap(short, long)]
+    pub break_time: u8,
+    
+    /// This argument takes in a list of comma seperated tasks
+    #[clap()]
+    pub block_distraction: bool,
+    
+    /// This argument takes in a list of comma seperated tasks
     pub tasks: Vec<String>
 }
 
-pub mod PomodoroCRUD {
-    impl crate::InterfacePomodoro {
-    pub fn new(work_time_minutes: u8, break_time_minutes: u8, tasks: Vec<String>) -> crate::InterfacePomodoro {
-        crate::InterfacePomodoro {
-            work_time_minutes,
-            break_time_minutes,
-            tasks,
+//pub struct Handler {
+//    handle: u8,
+//    next: Option<Handler>,
+//}
+
+impl InterfacePomodoro {
+    pub fn handle(&self) {
+        let _reslt = handlers::alerts::start(&self);
+        
+   } 
+}
+
+
+// Pomodoro Lifetime
+// Start
+// During
+// End
+//
+// At Start:
+// 1. Initial Notification is given on the desktop
+// 2. The /etc/hosts is overriden by the symlink
+// 3. Either triggered by the command or the program itself after the break
+//
+// During:
+// 1. Mid time the work time a notification is given to stay on track 
+//
+// End:
+// 1. SymLink is broken
+// 2. notification is given to the user
+
+
+// Create time based chain of responsibility for lifecycle of a pomodoro
+
+mod handlers {
+    pub mod alerts{
+        pub fn start(args: &crate::InterfacePomodoro) -> Result<(), Box<dyn std::error::Error>> {
+            // This gives a persistent notification till the time yo are working
+            // To remind you to not waste time
+            let body = format!("You need to focus on your work for next {} minutes.", args.work_time); 
+            crate::Notification::new()
+            .summary("Your Pomodoro started")
+            .body(&body)
+            .hint(crate::Hint::Resident(true)) 
+            .timeout(crate::Timeout::Never)
+            .show()?;
+            Ok(())
+        }
+
+        pub fn during(args: &crate::InterfacePomodoro)  -> Result<(), Box<dyn std::error::Error>> {
+            let body = format!("You need to focus on your work for next {} minutes.", args.work_time / 2); 
+            crate::Notification::new()
+            .summary("You are half way through ! Keep on the good work")
+            .body(&body)
+            .hint(crate::Hint::Resident(true)) 
+            .timeout(crate::Timeout::Never)
+            .show()?;
+            Ok(())
+        }
+
+        pub fn end(args: &crate::InterfacePomodoro)  -> Result<(), Box<dyn std::error::Error>> {
+            let body = format!("Your timer for {} minutes, is complete", args.work_time); 
+            crate::Notification::new()
+            .summary("You have completed the pomodoro.")
+            .body(&body)
+            .hint(crate::Hint::Resident(true)) 
+            .timeout(crate::Timeout::Never)
+            .show()?;
+            Ok(())
+    }
+    pub mod hosts{
+        pub fn block(args: &crate::InterfacePomodoro){
+            if args.block_distraction {
+                let _result = crate::symlink::symlink_file("/etc/hosts", "");
+            }
+        }
+        pub fn unblock(args: &crate::InterfacePomodoro){
+            let _result = crate::symlink::remove_symlink_dir("/etc/hosts");
         }
     }
-
-    pub fn update_break_time(&mut self, break_time_minutes: u8){
-        self.break_time_minutes = break_time_minutes;
-    }
-    pub fn update_work_time(&mut self, work_time_minutes: u8){
-        self.work_time_minutes = work_time_minutes;
-    }
 }
 }
-
-
-mod csv_operations {}
-
-mod block_distraction {
-    // Blocks distracting apps using /etc/hosts
-    pub fn block(){}
-    pub fn unblock(){}
-}
-mod alerts { 
-    pub fn alert(){}
-    pub fn progress_bar(){}
-    pub fn minimal(){}
-    pub fn motivate(){}
+mod logging{
+    // This is done to log your pomodoro sessions and the tasks that you did dring them
 }
 
-mod tui {
-    pub fn time_input_screen(){}
-    pub fn goals_input_screen(){}
-    pub fn focus_screen(){}
-    pub fn update_settings_screen(){}
-}
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,7 @@
-use cursive::Cursive;
-use cursive::views::{Button, Dialog, DummyView, TextView, LinearLayout, SelectView};
-use cursive::traits::*;
+use clap::Parser;
 
 fn main() {
-    let mut siv = cursive::default();
-    siv.add_layer(Dialog::around(TextView::new("Pomodoro"))
-        .button("Quit", |s| s.quit())
-        .button("Start", start_pomodoro));
-    siv.run();
+    let args = rust_pomodoro::InterfacePomodoro::parse();
+    args.handle();
 }
-
-fn build_root<'a>(siv: &'a mut Cursive, pomo: &str) -> &'a mut Cursive{
-    siv.pop_layer();
-    let select = SelectView::<String>::new()
-        .on_submit(on_submit)
-        .with_name("select");
-
-    let buttons = LinearLayout::vertical()
-        .child(Button::new("Add Task", add_task))
-        .child(Button::new("Delete Task", delete_task))
-        .child(Button::new("Start", start_timer))
-        .child(Button::new("Pause", pause_timer))
-        .child(Button::new("Reset timer",reset_timer))
-        .child(DummyView)
-        .child(Button::new("Quit", Cursive::quit));
-    siv.add_layer(Dialog::around(LinearLayout::horizontal()
-            .child(select)
-            .child(DummyView)
-            .child(buttons)).title(pomo));
-    siv
-}
-
-fn start_pomodoro<'a>(siv: &'a mut Cursive) -> (){
-    let siv_start = build_root(siv,  "00:00");
-    //siv_start
-}
-
-fn add_task(siv: &mut Cursive){}
-
-fn on_submit(siv: &mut Cursive, name: &str){}
-
-fn delete_task(siv: &mut Cursive){
-}
-
-fn reset_timer(siv: &mut Cursive){
-    let start_timer_siv = build_root(siv, "Reset Pressed");
-}
-fn start_timer(siv: &mut Cursive){
-    let start_timer_siv = build_root(siv, "Start Pressed");
-}
-
-fn pause_timer(siv: &mut Cursive){
-    let start_timer_siv = build_root(siv, "Pause Pressed");
-}
+// % cargo run -- --work-time 10 --break-time 10 -- --tasks a b c
